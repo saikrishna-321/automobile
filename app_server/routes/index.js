48396640002eddaa151e6a2dff2248a5c50c0f07
saikrishna-321 <21eg107b37@anurag.edu.in>
2023-09-21 14:12:32 +0530

    tr

---
+++ app_server/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
+/* LOCATIONS.js */
+router.get('/', ctrlLocations.homelist);
+router.get('/locations',ctrlLocations.locationInfo);
+router.get('/locations/review/new',ctrlLocations.addReview);
+/*OTHERS.js*/
+router.get('/about',ctrlOthers.about);
+
+module.exports = router;
